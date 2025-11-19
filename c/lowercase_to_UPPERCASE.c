51571bbf89dd1ac73949cae99115a6a8c9b28d56
kiran1929 <gudepukirandeep@gmail.com>
2025-11-19 10:49:38 +0530

    Create lowercase_to_UPPERCASE.c

---
+++ c/lowercase_to_UPPERCASE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char lower_case;
+    char upper_case;
+    scanf("%c",&lower_case);
+    upper_case=lower_case-32;
+    printf("The uppercase of %c is %c",lower_case,upper_case);
+    return 0;
+}
