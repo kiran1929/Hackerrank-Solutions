d7fb2e7a10c9748fd7fa907567c523f5ece68dfc
kiran1929 <gudepukirandeep@gmail.com>
2025-11-20 23:28:35 +0530

    Add Area of a Rectangle calculation in C

---
+++ c/Area_of_a_Rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int area(int length, int width)
+     {
+         return length*width;
+     }
+
+int main() {
+
+    int length_input,width_input;
+    scanf("%d %d",&length_input,&width_input);
+    int area_of_rectangle=area(length_input,width_input);
+    printf("The area is: %d units",area_of_rectangle);
+    return 0;
+}
