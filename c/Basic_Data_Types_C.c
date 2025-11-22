43bb6726276ab7a1db4be8316e20b69df2029f66
kiran1929 <gudepukirandeep@gmail.com>
2025-11-23 00:33:19 +0530

    Add Basic Data Types C program

---
+++ c/Basic_Data_Types_C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int int_var;
+    long long_var;
+    char char_var;
+    float float_var;
+    double double_var;
+  
+    scanf("%d %ld %c %f %lf",&int_var,&long_var,&char_var,&float_var,&double_var);
+  
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf",int_var,long_var,char_var,float_var,double_var);
+  
+    return 0;
+}
