e0fc3e7d74647e87a409e229981ac96e9ce876f1
kiran1929 <gudepukirandeep@gmail.com>
2025-12-04 22:22:54 +0530

    Create Discounted_Price.c
    Implemented a program that calculates the final price of a product after
    applying a discount percentage. The logic is handled through a separate
    function (discounted) which computes the discount amount and returns the
    reduced price. The user inputs the original price and discount percentage,
    and the program outputs the final price after the discount is applied.

---
+++ c/Discounted_Price.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+    float discounted(float price, float percent){
+        float discountAmount = ( price * percent ) / 100;
+
+        float discountedPrice = price - discountAmount;
+        return discountedPrice;
+    }
+int main() {
+    float price,percent;
+    scanf("%f%f",&price,&percent);
+    float amount=discounted(price,percent);
+    printf("The final price is: %f",amount);   
+    return 0;
+}
