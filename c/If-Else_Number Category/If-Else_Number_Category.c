1491c4aad95fc9d31ab60384cf6a60c94e3ffddf
kirandeep <gudepukirandeep@gmail.com>
2025-12-18 23:02:30 +0530

    Create If-Else_Number_Category.c

---
+++ c/If-Else_Number Category/If-Else_Number_Category.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n,square;
+    // int a = 2|| 3|| 5|| 7|| 11;
+    // int b = 2|| 3|| 5|| 7|| 11;
+    scanf("%d",&n);
+    square=n*n;
+    if ( (n%2==0 && n%3==0) && n%5!=0)
+    {
+        printf("A");
+    }
+    else if( (n > 20 &&
+            (n == 2*3  || n == 2*5  || n == 2*7  || n == 2*11 ||
+             n == 3*5  || n == 3*7  || n == 3*11 ||
+             n == 5*7  || n == 5*11 ||
+             n == 7*11))){
+        printf("B");
+    }
+    else if(n %3==0 ^ n%7==0 && ( n > 10 && n < 99))
+    {
+        printf("C");
+    }
+    else
+    {
+        int sq = (int)sqrt(n);
+        if (sq * sq == n && n < 200) {
+            printf("D");
+        }
+        else
+        {
+            printf("E");
+        }
+    }
+    
+    
+    return 0;
+}
