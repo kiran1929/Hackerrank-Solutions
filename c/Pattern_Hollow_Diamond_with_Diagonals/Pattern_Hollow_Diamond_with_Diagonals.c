d5f28cce13e4888238803885432fbcd027512d43
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-01-10 00:35:07 +0530

    Add hollow diamond pattern with diagonals in C

---
+++ c/Pattern_Hollow_Diamond_with_Diagonals/Pattern_Hollow_Diamond_with_Diagonals.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if (n <= 1)
+    {
+        printf("Shape Not Possible");
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (int k = 1; k <= 2*i - 1; k++)
+        {
+            if (i == n || k == 1 || k == i || k == 2*i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (int k = 1; k <= 2*i - 1; k++)
+        {
+            if (k == 1 || k == i || k == 2*i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
