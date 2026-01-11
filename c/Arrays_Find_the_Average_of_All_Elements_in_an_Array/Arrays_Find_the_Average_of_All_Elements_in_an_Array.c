48c6969cfcca0345d80a3cfda6b5f779803dad3b
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-01-11 21:57:45 +0530

    Implement average calculation for array elements

---
+++ c/Arrays_Find_the_Average_of_All_Elements_in_an_Array/Arrays_Find_the_Average_of_All_Elements_in_an_Array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    long long arr[n];
+    long long sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    
+    double avg = (double)sum / n;
+    printf("%.6f", avg);
+
+    return 0;
+}
