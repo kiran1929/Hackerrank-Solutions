921c2931c4a2b25221350dc7f19d9c319fb89be0
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-01-19 00:18:37 +0530

    Implement program to count elements above average

---
+++ c/Arrays_Count_the_Number_of_Elements_Greater_Than_the_Average_Value/Arrays_Count_the_Number_of_Elements_Greater_Than_the_Average_Value.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int n,i,find=0;
+    long long sum=0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++){
+        sum+=arr[i];
+    }
+    double avg=(double)sum/n;
+    for (i = 0; i < n; i++){
+        if (arr[i] > avg) {
+            ++find;
+        }
+    }
+    printf("%d",find);
+
+    return 0;
+}
